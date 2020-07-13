6594699822a1ad2e063a4d2a7ca6bfae5b36b63a
mawenzhong <546591791@qq.com>
2020-07-14 00:58:02 +0800

    add

--- day1/ch1_test.go
+++ day1/ch1_test.go
@@ -1,7 +1,43 @@
 package day1
 
-import "testing"
+import (
+	"fmt"
+	"math"
+	"testing"
+	"time"
+)
 
 func TestCh1(t *testing.T) {
 	t.Log("hello world")
+}
+
+type T struct {
+	name  string // name of the object
+	value int    // its value
+}
+
+func (t *T) toString() {
+	fmt.Println(fmt.Sprintf("name=%s, value=%d", t.name, t.value))
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func TestCh2(t *testing.T) {
+	t.Log("The time is", time.Now())
+	t.Log(add(1, 2))
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func TestVertex(t *testing.T) {
+	v := Vertex{3, 4}
+	t.Log(v.Abs())
 }
